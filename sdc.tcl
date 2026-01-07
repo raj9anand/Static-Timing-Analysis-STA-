5e6a87e9736680659396938a1107dfc617ed71f3
Anand Raj <60174790+raj9anand@users.noreply.github.com>
2026-01-07 19:34:54 +0530

    Delete sdc.tcl

--- sdc.tcl
+++
@@ -1,7 +0,0 @@
-create_clock -name clk -period 10 [get_ports clk]
-set_input_delay 2 -clock clk [get_ports data_in]
-set_output_delay 3 -clock clk [get_ports data_out]
-set_clock_uncertainty 0.2 clk
-set_false_path -from [get_clocks clk1] -to [get_clocks clk2]
-set_multicycle_path 2 -from reg1 -to reg2
-
